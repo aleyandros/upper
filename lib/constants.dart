e153582899ffa487cd2e27ba8d7c369fd96eefa6
aleyandros <ayopatincha@gmail.com>
2020-07-19 16:54:07 -0500

    Added Constants

---
+++ lib/constants.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+const kYellowColour = Color(0xFFFFB800);
+const kBlueColour = Color(0xFF2BB2FF);
+const kWhiteColour = Color(0xFFFFFFFF);
+const kGreyColour = Color(0xFFB1B1B1);
+const kBlackColour = Color(0xFF000000);
+const kRedColour = Color(0xFFE30000);
+
+const kLabelTitleWhite = TextStyle(
+  fontSize: 25.0,
+  color: kWhiteColour,
+  fontFamily: 'Montserrat-SemiBold',
+);
+
+const kLabelSubtitleWhite = TextStyle(
+  fontSize: 16.0,
+  color: kWhiteColour,
+  fontFamily: 'Montserrat-Regular',
+);
+
+const kLabelWhite = TextStyle(
+  fontSize: 14.0,
+  color: kWhiteColour,
+  fontFamily: 'Montserrat-Regular',
+);
+
+const kLabelButtonWhite = TextStyle(
+  fontSize: 12.0,
+  color: kWhiteColour,
+  fontFamily: 'Montserrat-Medium',
+);
+
+const kLabelBlue = TextStyle(
+  fontSize: 14.0,
+  color: kBlueColour,
+  fontFamily: 'Montserrat-Regular',
+);
+
+const kLabelUnderlineBlue = TextStyle(
+  fontSize: 14.0,
+  color: kBlueColour,
+  fontFamily: 'Montserrat-Medium',
+  decoration: TextDecoration.underline,
+);
+
+const kLabelTitleBlack = TextStyle(
+  fontSize: 16.0,
+  color: kBlackColour,
+  fontFamily: 'Montserrat-Medium',
+);
+
+const kLabelSubtitleBlack = TextStyle(
+  fontSize: 12.0,
+  color: kBlackColour,
+  fontFamily: 'Montserrat-Regular',
+);
+
+const kLabelBlack = TextStyle(
+  fontSize: 14.0,
+  color: kBlackColour,
+  fontFamily: 'Montserrat-Light',
+);
+
+const kLabelExampleGrey = TextStyle(
+  fontSize: 14.0,
+  color: kGreyColour,
+  fontFamily: 'Montserrat-Regular',
+);
+
+const kLabelClosedRed = TextStyle(
+  fontSize: 12.0,
+  color: kRedColour,
+  fontFamily: 'Montserrat-Regular',
+);
