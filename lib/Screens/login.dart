08780f8ed90fbfa8d2bf6545508aaa2a9155104f
juanheri <juanheri@unisabana.edu.co>
2020-07-25 00:09:13 -0500

    login prácticamente terminado

--- lib/Screens/login.dart
+++ lib/Screens/login.dart
@@ -21,23 +21,6 @@ class _LoginState extends State<Login> {
                   child: Container(
                     width: double.infinity,
                     color: kBlueColour,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text(
-                          'Bienvenido a Upper',
-                          style: kLabelTitleWhite,
-                        ),
-                        SizedBox(
-                          height: 10.0,
-                        ),
-                        Text(
-                          'Inicia sesion o crea una\ncuenta.',
-                          textAlign: TextAlign.center,
-                          style: kLabelSubtitleWhite,
-                        ),
-                      ],
-                    ),
                   ),
                 ),
                 Expanded(
@@ -46,28 +29,161 @@ class _LoginState extends State<Login> {
                     color: kWhiteColour,
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text(
-                          '¿No tienes cuenta? ',
-                          style: kLabelBlue,
-                        ),
-                        Text(
-                          'crea una',
-                          style: kLabelUnderlineBlue,
-                        )
-                      ],
                     ),
                   ),
                 ),
               ],
             ),
-            Positioned(
-              top: 395.0,
-              child: Container(
-                color: kWhiteColour,
-                width: 374.0,
-                height: 200.0,
-              ),
+            Column(
+              children: <Widget>[
+                Expanded(
+                  flex: 6,
+                  child: SizedBox(),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Text(
+                    'Bienvenido a Upper',
+                    style: kLabelTitleWhite,
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Text(
+                    'Inicia sesión o crea una\ncuenta.',
+                    textAlign: TextAlign.center,
+                    style: kLabelSubtitleWhite,
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: SizedBox(),
+                ),
+                Expanded(
+                  flex: 8,
+                  child: Card(
+                    child: Stack(
+                      alignment: AlignmentDirectional.bottomCenter,
+                      overflow: Overflow.visible,
+                      children: <Widget>[
+                        Container(
+                          color: Colors.white,
+                          width: 374.0,
+                          child: Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: Column(
+                              children: <Widget>[
+                                Expanded(
+                                  flex: 1,
+                                  child: SizedBox(),
+                                ),
+                                Expanded(
+                                  flex: 3,
+                                  child: Column(
+                                    children: <Widget>[
+                                      Align(
+                                        alignment: Alignment.centerLeft,
+                                        child: Text(
+                                          'Correo Electronico:',
+                                          style: kLabelSignupBlue,
+                                        ),
+                                      ),
+                                      TextFormField(
+                                        decoration: const InputDecoration(
+                                          hintText: 'diego35.da@gmail.com',
+                                        ),
+                                        validator: (value) {
+                                          if (value.isEmpty) {
+                                            return 'ex: upper@mail.co';
+                                          }
+                                          return null;
+                                        },
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Expanded(
+                                  flex: 3,
+                                  child: Column(
+                                    children: <Widget>[
+                                      Align(
+                                        alignment: Alignment.centerLeft,
+                                        child: Text(
+                                          'Contraseña:',
+                                          style: kLabelSignupBlue,
+                                        ),
+                                      ),
+                                      TextFormField(
+                                        decoration: const InputDecoration(
+                                          hintText: '**********',
+                                        ),
+                                        validator: (value) {
+                                          if (value.isEmpty) {
+                                            return '******';
+                                          }
+                                          return null;
+                                        },
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Expanded(
+                                  flex: 1,
+                                  child: SizedBox(),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          bottom: -25,
+                          child: SizedBox(
+                            width: 314,
+                            height: 50,
+                            child: RaisedButton(
+                              color: kYellowColour,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius:
+                                  new BorderRadius.circular(15.0)),
+                              onPressed: () {
+
+                              },
+                              child: Text(
+                                'INICIAR SESIÓN',
+                                style: kLabelButtonWhite,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: SizedBox(),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        '¿No tienes cuenta? ',
+                        style: kLabelBlue,
+                      ),
+                      Text(
+                        "crea una",
+                        style: kLabelUnderlineBlue,
+                      )
+                    ],
+                  ),
+                ),
+                Expanded(
+                  flex: 5,
+                  child: SizedBox(),
+                ),
+              ],
             ),
           ],
         ),
