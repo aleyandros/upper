47839f8a528d83211bb6e236e6be4db7c1b130b6
aleyandros <ayopatincha@gmail.com>
2020-08-15 01:11:24 -0500

    Added Logo

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: Categories.id,
+      initialRoute: Index.id,
       routes: {
         Login.id: (context) => Login(),
         Signup.id: (context) => Signup(),
